985a1f9cf377257768e72c0957cccd1498efc3b0
anglodata <141818029+anglodata@users.noreply.github.com>
2023-12-22 15:05:09 +0100

    variables created by name

--- optim.py
+++ optim.py
@@ -1,5 +1,9 @@
 from ortools.linear_solver import pywraplp
 import numpy as np
+import sys
+import pandas as pd
+
+
 
 def main():
     # Create the linear solver with the GLOP backend.
@@ -7,19 +11,39 @@ def main():
     if not solver:
         return
 
-    # Create the variables x and y.
-    x = solver.NumVar(0, solver.infinity(), "x")
-    y = solver.NumVar(0, solver.infinity(), "y")
-    z = solver.NumVar(0, solver.infinity(), "z")
+
+    
+
+    # set variable names with strings
+    # Create the variables x and y. (equivalent to :)
+    # x = solver.NumVar(0, solver.infinity(), "x")
+    # y = solver.NumVar(0, solver.infinity(), "y")
+    # z = solver.NumVar(0, solver.infinity(), "z")
+
+    my_vars = sys.argv[1:]
+
+
+
+    for my_var in my_vars:
+      globals()[my_var] = solver.NumVar(0, solver.infinity(), my_var)
 
     print("Number of variables =", solver.NumVariables())
-
+    
     # Create a linear constraint, 0 <= x + y + z<= 
 
-
-    perf_theo = np.array([150,100,80],dtype="float")
+    perf_theo = np.array([150, 100, 80],dtype="float")
     perf_theo_half_month = np.array([2*x for x in perf_theo],dtype="float")
     realized = np.array([100,110,70],dtype="float")
+
+    df = pd.read_csv("monthly_objectives.csv")
+
+    perf_theo = df["perf_theo_10"]
+    perf_theo_half_month = df["perf_theo_20"]
+    realized_10 = df["realized_10"]
+
+    assert len(perf_theo) == len(perf_theo_half_month)
+    assert len(perf_theo) == len(realized_10)
+    assert len(perf_theo) == len(my_vars)
 
     # performance theorique a 20 j:
     # 2x fois la performance hebdo - réalisé
@@ -43,9 +67,9 @@ def main():
 
 
     ct = solver.Constraint(lower_bound, upper_bound, "ct")
-    ct.SetCoefficient(x, 1)
-    ct.SetCoefficient(y, 1)
-    ct.SetCoefficient(z, 1)
+    ct.SetCoefficient(globals()[my_vars[0]], 1)
+    ct.SetCoefficient(globals()[my_vars[1]], 1)
+    ct.SetCoefficient(globals()[my_vars[2]], 1)
 
     # delta = perf hebdo vs produced
     coef = perf_theo - realized
@@ -57,17 +81,17 @@ def main():
     ctx = solver.Constraint(0.5*(perf_theo_half_month[0]-realized[0]), 
                             min((2*perf_theo[0] - realized[0])*((coef[0]*1.05) + (1 - coef[0])*1.0), perf_theo[0]*1.05), 
                             "ctx")
-    ctx.SetCoefficient(x, 1)
+    ctx.SetCoefficient(globals()[my_vars[0]], 1)
 
     cty = solver.Constraint(0.5*(perf_theo_half_month[1]-realized[1]), 
                             min((2*perf_theo[1] - realized[1])*((coef[1]*1.05) + (1 - coef[1])*1.0), perf_theo[1]*1.05), 
                             "cty")
-    cty.SetCoefficient(y, 1)
+    cty.SetCoefficient(globals()[my_vars[1]], 1)
 
     ctz = solver.Constraint(0.5*(perf_theo_half_month[2]-realized[2]), 
                             min((2*perf_theo[2] - realized[2])*((coef[2]*1.05) + (1 - coef[2])*1.0), perf_theo[2]*1.05), 
                             "ctz")
-    ctz.SetCoefficient(z, 1)
+    ctz.SetCoefficient(globals()[my_vars[2]], 1)
 
 
     print("Number of constraints =", solver.NumConstraints())
@@ -75,20 +99,19 @@ def main():
     # Create the objective function, x + y + z.
     # we want to maximize the number of produced units for each laboratory
     objective = solver.Objective()
-    objective.SetCoefficient(x, 1)
-    objective.SetCoefficient(y, 1)
-    objective.SetCoefficient(z, 1)
-    objective.SetMaximization()
+
+    for m_var in my_vars:
+      objective.SetCoefficient(globals()[m_var], 1)
+      objective.SetMaximization()
 
     print(f"Solving with {solver.SolverVersion()}")
     solver.Solve()
 
     print("Solution:")
     print("Objective value =", objective.Value())
-    print("x =", x.solution_value())
-    print("y =", y.solution_value())
-    print("z =", z.solution_value())
-
+    for m_var in my_vars:
+      print(f"{m_var} =", globals()[m_var].solution_value())
+    
 
 if __name__ == "__main__":
     main()
